92dfd093dca5aa0a005f19d7ff18dfdfec9a1ba8
Antonio Alex <antonio.arnj@gmail.com>
2025-12-02 17:11:40 -0300

    Fix

--- next.config.mjs
+++ next.config.mjs
@@ -4,14 +4,14 @@ const isProd = process.env.NODE_ENV === 'production'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // necessário para GitHub Pages
-  basePath: isProd ? '/PortfolioTest' : '',
-  assetPrefix: isProd ? '/PortfolioTest/' : '',
+  basePath: isProd ? '/ra1461392111005' : '',
+  assetPrefix: isProd ? '/ra1461392111005/' : '',
   images: {
     unoptimized: true, // GitHub Pages não suporta otimizador de imagens
   },
   trailingSlash: true, // gera URLs com barra no final (evita 404 no Pages)
   env: {
-    NEXT_PUBLIC_BASE_PATH: isProd ? '/PortfolioTest' : '',
+    NEXT_PUBLIC_BASE_PATH: isProd ? '/ra1461392111005' : '',
   },
 }
 
